de161a7004ee811dc0e4c9f09cd7a45b29e058f4
yung988 <yeezuz332@gmail.com>
2025-08-23 22:13:32 +0200

    fix route handler signatures for Next.js 15

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,8 +13,8 @@ const defaultUrl = process.env.VERCEL_URL
 
 export const metadata: Metadata = {
   metadataBase: new URL(defaultUrl),
-  title: "Next.js and Supabase Starter Kit",
-  description: "The fastest way to build apps with Next.js and Supabase",
+  title: "Yeezuz2020 Store",
+  description: "2020",
 };
 
 const geistSans = Geist({
